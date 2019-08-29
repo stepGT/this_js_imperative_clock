036149aec5e6cdb86bcf10c2cf3560974e3d7595
Andrey <stepgt@mail.ru>
2019-08-29 16:10:39 +1000

    Prepend a 0 on the hour minutes seconds

--- clock.js
+++ clock.js
@@ -34,6 +34,18 @@ function getClock() {
     time.f = "PM";
     time.h -= 12;
   }
-  // 
+  // Prepend a 0 on the hours to make double digits
+  if (time.hours < 10) {
+    time.hours = "0" + time.hours;
+  }
+  // prepend a 0 on the minutes to make double digits
+  if (time.minutes < 10) {
+    time.minutes = "0" + time.minutes;
+  }
+  // prepend a 0 on the seconds to make double digits
+  if (time.seconds < 10) {
+    time.seconds = "0" + time.seconds;
+  }
+  // Format the clock time as a string "hh:mm:ss tt"
   return time.h + ":" + time.m + ":" + time.s + " " + time.f;
 }
